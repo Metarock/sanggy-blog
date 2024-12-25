9a1a9858ffacefd1b34a084e864f34a781ad580f
John Isaiah Sangalang <66818636+Metarock@users.noreply.github.com>
2024-12-26 01:53:13 +1300

    Fix/hotfixes (#3)
    * task: include placeholder
    
    * fix: placeholder

--- src/components/BlogPostPreview.tsx
+++ src/components/BlogPostPreview.tsx
@@ -1,39 +1,43 @@
-"use client";
-import { cn } from "@/lib/utils";
-import { GetPostsResult } from "@/lib/wisp";
-import { formatDate } from "date-fns";
-import Image from "next/image";
-import Link from "next/link";
-import { FunctionComponent } from "react";
+'use client';
+import { cn } from '@/lib/utils';
+import { GetPostsResult } from '@/lib/wisp';
+import { formatDate } from 'date-fns';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FunctionComponent } from 'react';
 
 export const BlogPostPreview: FunctionComponent<{
-  post: GetPostsResult["posts"][0];
+  post: GetPostsResult['posts'][0];
 }> = ({ post }) => {
   return (
     <div className="break-words">
       <Link href={`/blog/${post.slug}`}>
-        <div className="aspect-[16/9] relative">
+        <div className="relative aspect-[16/9]">
           <Image
             alt={post.title}
             className="object-cover"
-            src={post.image || "/images/placeholder.webp"}
+            src={post.image || '/images/placeholder.webp'}
             fill
+            priority
           />
         </div>
       </Link>
-      <div className="grid grid-cols-1 gap-3 md:col-span-2 mt-4">
-        <h2 className="font-sans font-semibold tracking-tighter text-primary text-2xl md:text-3xl">
+      <div className="mt-4 grid grid-cols-1 gap-3 md:col-span-2">
+        <h2 className="font-sans text-2xl font-semibold tracking-tighter text-primary md:text-3xl">
           <Link href={`/blog/${post.slug}`}>{post.title}</Link>
         </h2>
-        <div className="prose lg:prose-lg italic tracking-tighter text-muted-foreground">
-          {formatDate(post.publishedAt || post.updatedAt, "dd MMMM yyyy")}
+        <div className="prose italic tracking-tighter text-muted-foreground lg:prose-lg">
+          {formatDate(post.publishedAt || post.updatedAt, 'dd MMMM yyyy')}
         </div>
-        <div className="prose lg:prose-lg leading-relaxed md:text-lg line-clamp-4 text-muted-foreground">
+        <div className="prose line-clamp-4 leading-relaxed text-muted-foreground lg:prose-lg md:text-lg">
           {post.description}
         </div>
         <div className="text-sm text-muted-foreground">
           {post.tags.map((tag) => (
-            <div key={tag.id} className="mr-2 inline-block">
+            <div
+              key={tag.id}
+              className="mr-2 inline-block"
+            >
               <Link href={`/tag/${tag.name}`}>#{tag.name}</Link>
             </div>
           ))}
@@ -44,18 +48,21 @@ export const BlogPostPreview: FunctionComponent<{
 };
 
 export const BlogPostsPreview: FunctionComponent<{
-  posts: GetPostsResult["posts"];
+  posts: GetPostsResult['posts'];
   className?: string;
 }> = ({ posts, className }) => {
   return (
     <div
       className={cn(
-        "grid grid-cols-1 gap-16 lg:gap-28 md:grid-cols-2 md:my-16 my-8",
-        className
+        'my-8 grid grid-cols-1 gap-16 md:my-16 md:grid-cols-2 lg:gap-28',
+        className,
       )}
     >
       {posts.map((post) => (
-        <BlogPostPreview key={post.id} post={post} />
+        <BlogPostPreview
+          key={post.id}
+          post={post}
+        />
       ))}
     </div>
   );
--- src/components/RelatedPosts.tsx
+++ src/components/RelatedPosts.tsx
@@ -1,37 +1,43 @@
-"use client";
+'use client';
 
-import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-import type { GetRelatedPostsResult } from "@wisp-cms/client";
-import Image from "next/image";
-import Link from "next/link";
-import type { FunctionComponent } from "react";
+import { AspectRatio } from '@radix-ui/react-aspect-ratio';
+import type { GetRelatedPostsResult } from '@wisp-cms/client';
+import Image from 'next/image';
+import Link from 'next/link';
+import type { FunctionComponent } from 'react';
 
 export const RelatedPosts: FunctionComponent<{
-  posts: GetRelatedPostsResult["posts"];
+  posts: GetRelatedPostsResult['posts'];
 }> = ({ posts }) => {
   if (posts.length === 0) {
     return null;
   }
 
   return (
-    <div className="my-8 max-w-prose text-xl mx-auto">
+    <div className="mx-auto my-8 max-w-prose text-xl">
       <div className="mb-6 text-lg font-semibold tracking-tight">
         Related Posts
       </div>
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
         {posts.slice(0, 3).map((post) => (
-          <div className=" bg-muted overflow-hidden rounded-lg" key={post.id}>
+          <div
+            className="overflow-hidden rounded-lg bg-muted"
+            key={post.id}
+          >
             <Link href={`/blog/${post.slug}`}>
-              <AspectRatio ratio={16 / 9} className="w-full">
+              <AspectRatio
+                ratio={16 / 9}
+                className="w-full"
+              >
                 <Image
-                  src={post.image || "/images/placeholder.png"}
+                  src={post.image || '/images/placeholder.webp'}
                   alt={post.title}
                   fill
                   className="h -full min-h-full min-w-full object-cover object-center"
                 />
               </AspectRatio>
             </Link>
-            <div className="prose prose-sm dark:prose-invert p-4">
+            <div className="prose prose-sm p-4 dark:prose-invert">
               <h3 className="line-clamp-2">{post.title}</h3>
               <p className="line-clamp-3">{post.description}</p>
               <Link href={`/blog/${post.slug}`}>
