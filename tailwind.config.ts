38d731777eb3c0acfc5825d9ddd455ebc020391e
John Isaiah Sangalang <esang037@gmail.com>
2024-12-26 07:26:40 +0900

    fix: tailwindcss order

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,13 @@
 import type { Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 
+// content: [
+//   "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//   "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//   "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+//   "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   "./src/app/styles/**/*.css",
+// ],
 export default {
   darkMode: ['class'],
   content: [
@@ -8,6 +15,7 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{ts,tsx}',
+    './src/app/*.css',
   ],
   prefix: '',
   theme: {
