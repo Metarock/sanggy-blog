f56e9c6d7d694ebd7348db0e7b6a328ec0af061a
John Isaiah Sangalang <66818636+Metarock@users.noreply.github.com>
2024-12-25 22:46:17 +1300

    San 12 setup homepage
    * task: install more package
    
    * task: install more package
    
    * wip: link wisp cms
    
    * fix: suppress hydration warning
    
    * task: reset lock file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imagedelivery.net",
+      },
+    ],
+  },
+  redirects: async () => {
+    return [];
+  },
 };
 
 export default nextConfig;
