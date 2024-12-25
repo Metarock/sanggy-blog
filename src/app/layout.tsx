bb0d0e1844e7448c019638e032df73bce3792127
John Isaiah Sangalang <esang037@gmail.com>
2024-12-25 18:18:06 +0900

    wip: link wisp cms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,29 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { config } from "@/config";
+import { signOgImageUrl } from "@/lib/og-image";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
+const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
 export const metadata: Metadata = {
-  title: "John's Blog",
-  description:
-    "A blog about my life, adventures, and thoughts. Might be interesting, might not be. Who knows? 🤷‍♂️",
+  title: {
+    absolute: config.blog.metadata.title.absolute,
+    default: config.blog.metadata.title.default,
+    template: config.blog.metadata.title.template,
+  },
+  description: config.blog.metadata.description,
+  openGraph: {
+    title: config.blog.metadata.title.default,
+    description: config.blog.metadata.description,
+    images: [
+      signOgImageUrl({
+        title: config.blog.name,
+      }),
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -14,7 +33,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased max-w-6xl m-auto",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main>{children}</main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
