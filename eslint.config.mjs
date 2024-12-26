8e410b8ab4d91d2c7c87c843c56b2676cff1bc4c
John Isaiah Sangalang <66818636+Metarock@users.noreply.github.com>
2024-12-26 13:09:33 +1300

    Fix/eslint tailwindcss (#9)
    * fix: tailwindcss order
    
    * refactor: update eslint file
    
    * task: update prettier config
    
    * fix: more fucking lint

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+// eslint.config.mjs
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+/** @type {import('eslint').Linter.Config[]} */
+const configs = [
+  ...compat.config({
+    root: true,
+    ignorePatterns: [
+      'node_modules',
+      '.next',
+      'out',
+      '/pnpm-lock.yaml',
+      '/yarn.lock',
+      '/.git',
+      '/.cache',
+      '/tailwind.config.ts',
+      '/postcss.config.cjs',
+    ],
+    extends: [
+      'plugin:prettier/recommended',
+      'plugin:tailwindcss/recommended',
+      'plugin:import/typescript',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@next/next/recommended',
+      'next/core-web-vitals',
+      'next/typescript',
+    ],
+    plugins: ['import', '@typescript-eslint', 'prettier', 'tailwindcss'],
+    rules: {
+      'import/no-default-export': 'error',
+    },
+    overrides: [
+      // App router
+      {
+        files: ['*.ts', '*.tsx', '*.js'],
+        parser: '@typescript-eslint/parser',
+      },
+      {
+        files: [
+          'src/pages/**/*',
+          'src/app/**/{page,layout,not-found,robots,sitemap,route}.{js,ts,jsx,tsx,mdx}',
+          'src/middleware.ts',
+          'src/loaders/**/*',
+          '{eslint,prettier,tailwind,postcss,next}.config.{mjs,ts}',
+        ],
+        rules: {
+          'import/no-default-export': 'off',
+        },
+      },
+    ],
+  }),
+];
+
+export default configs;
